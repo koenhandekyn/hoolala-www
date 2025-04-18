9509adde660d24dafb2bebaa6bc51190279d041b
Koen Handekyn <koen@handekyn.com>
2025-04-18 13:10:36 -0400

    redirect /p/* to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -9,6 +8,7 @@ import NotFound from "./pages/NotFound";
 import Summary from "./pages/Summary";
 import UseCasesPage from "./pages/UseCasesPage";
 import DPPPage from "./pages/DPPPage";
+import RedirectToSubdomain from "./components/RedirectToSubdomain";
 
 const queryClient = new QueryClient();
 
@@ -23,6 +23,7 @@ const App = () => (
           <Route path="/summary" element={<Summary />} />
           <Route path="/use-cases" element={<UseCasesPage />} />
           <Route path="/dpp" element={<DPPPage />} />
+          <Route path="/p/*" element={<RedirectToSubdomain />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
---
+++ src/components/RedirectToSubdomain.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const RedirectToSubdomain = () => {
+  const location = useLocation();
+  useEffect(() => {
+    const newUrl = `https://app.hoolala.app${location.pathname}${location.search}`;
+    window.location.href = newUrl;
+  }, [location]);
+  return null;
+};
+
+export default RedirectToSubdomain;
